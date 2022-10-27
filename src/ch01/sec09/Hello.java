05678d416577d7ddf3b0a0d925c7d184c6705ad4
ddongg <daeku7820566@gmail.com>
2022-10-27 03:08:39 -0700

    This is Commit message

---
+++ src/ch01/sec09/Hello.java
@@ -0,0 +1,11 @@
+package ch01.sec09;//package를 선언하겠다
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello,Java1");
+// main method, main method block.
+		System.out.println("Hello,Java2");
+	}
+
+}
